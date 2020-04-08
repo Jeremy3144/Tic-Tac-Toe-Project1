bcda406f36267aaf41cbad16576270ccf22e4207
Jeremy <jeremy.brockhoff@gmail.com>
2020-04-08 18:16:53 +1000

    90% of style done and reset and next game buttons fucntions done

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-console.log('awdwad');
-
 
 var gameBtns = document.querySelectorAll('.btn');
-
-// console.log(gameBtns);
-
-//handle a click depending on user and then display their marker - check
-// change players after click - check
-// needs to check to see if already used - check
-// adds there box to the players playedArray  - check
+var plyrAsignBtn = document.querySelector('.plyr-asign-btn')
+var nxtGameBtn = document.querySelector('.nxt-game-btn')
+var resetBtn = document.querySelector('.reset-btn')
 
 
 var isPlayer1 = true;
@@ -16,9 +10,11 @@ var boxesPlayed = [];
 var p1BoxsPlyd = [];
 var p2BoxsPlyd = [];
 var hasWon = false;
+var p1Score = 0;
+var p2Score = 0;
 // .includes the boxes played array for if it cn be used or not
 function handleClick(event) {
-  if (event.target.textContent === 'X' || event.target.textContent === 'O') {
+  if (event.target.textContent === 'X' || event.target.textContent === 'O' || hasWon === true) {
     return;
   }
 
@@ -54,7 +50,7 @@ var winningBoxes = [
   [3,6,9],
   [1,5,9],
   [3,5,7]
-]
+];
 
 function handleWinner() {
 
@@ -62,10 +58,12 @@ function handleWinner() {
     if (p1BoxsPlyd.includes(winningBoxes[i][0]) && p1BoxsPlyd.includes(winningBoxes[i][1]) && p1BoxsPlyd.includes(winningBoxes[i][2])) {
       console.log('player1 wins');
       hasWon = true;
+      p1Score += 1;
       
     } else if (p2BoxsPlyd.includes(winningBoxes[i][0]) &&          p2BoxsPlyd.includes(winningBoxes[i][1]) && p2BoxsPlyd.includes(winningBoxes[i][2])) {
       console.log('player2 wins');
       hasWon = true;
+      p2Score += 1;
 
     }
     
@@ -76,6 +74,33 @@ function handleWinner() {
   }
 }
 
+function handleNextGame() {
+  isPlayer1 = true;
+  boxesPlayed = [];
+  p1BoxsPlyd = [];
+  p2BoxsPlyd = [];
+  hasWon = false;
+  gameBtns.forEach(function(box){
+    box.textContent = ""
+  })
+}
+
+nxtGameBtn.addEventListener('click', handleNextGame);
+
+function handleReset() {
+  isPlayer1 = true;
+  boxesPlayed = [];
+  p1BoxsPlyd = [];
+  p2BoxsPlyd = [];
+  hasWon = false;
+  p1Score = 0;
+  p2Score = 0;
+  gameBtns.forEach(function(box){
+    box.textContent = ""
+  })
+}
+
+resetBtn.addEventListener('click', handleReset);
 
   
 
